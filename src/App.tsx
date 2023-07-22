90081032fd3fa010e161e77adba186a53c400ac7
Nandy Bâ <nandyba@yahoo.fr>
2023-07-23 01:36:05 +0200

    Add walletConnectV2Adapter

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import { Web3Auth } from "@web3auth/modal";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
 import "./App.css";
+
+// Plugins
+import { TorusWalletConnectorPlugin } from "@web3auth/torus-wallet-connector-plugin"
+
+
 import RPC from "./web3RPC"; // for using web3.js
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-// EVM
-import Web3 from "web3";
+
 // Solana
 import {
   SolanaPrivateKeyProvider,
@@ -26,6 +30,15 @@ import starkwareCrypto from "@starkware-industries/starkware-crypto-utils";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ec as elliptic } from "elliptic";
 
+import {
+  WalletConnectV2Adapter,
+  getWalletConnectV2Settings
+} from "@web3auth/wallet-connect-v2-adapter"
+import { MetamaskAdapter } from "@web3auth/metamask-adapter"
+import { TorusWalletAdapter } from "@web3auth/torus-evm-adapter"
+
+import config from "./config"
+
 const clientId =
   "BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk"; // get from https://dashboard.web3auth.io
 
@@ -33,6 +46,7 @@ function App() {
   const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
   const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
   const [loggedIn, setLoggedIn] = useState(false);
+  const [torusPlugin, setTorusPlugin] = useState<TorusWalletConnectorPlugin | null>(null);
 
   useEffect(() => {
     const init = async () => {
@@ -79,10 +93,163 @@ function App() {
     init();
   }, []);
 
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x1",
+            rpcTarget: "https://rpc.ankr.com/eth" // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+          uiConfig: {
+            appName: "W3A",
+            appLogo: "https://web3auth.io/images/w3a-L-Favicon-1.svg", // Your App Logo Here
+            theme: "light",
+            loginMethodsOrder: ["apple", "google", "twitter"],
+            defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+            loginGridCol: 3,
+            primaryButton: "externalLogin" // "externalLogin" | "socialLogin" | "emailLogin"
+          },
+          web3AuthNetwork: "cyan"
+        })
+
+        const openloginAdapter = new OpenloginAdapter({
+          loginSettings: {
+            mfaLevel: (config.environment === "development") ? 'none' : 'optional'
+          },
+          adapterSettings: {
+            uxMode: "popup", // "redirect" | "popup"
+            whiteLabel: {
+              name: "Your app Name",
+              logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+              logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+              defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+              dark: false // whether to enable dark mode. defaultValue: false
+            },
+            mfaSettings: {
+              deviceShareFactor: {
+                enable: true,
+                priority: 1,
+                mandatory: true
+              },
+              backUpShareFactor: {
+                enable: true,
+                priority: 2,
+                mandatory: false
+              },
+              socialBackupFactor: {
+                enable: true,
+                priority: 3,
+                mandatory: false
+              },
+              passwordFactor: {
+                enable: true,
+                priority: 4,
+                mandatory: false
+              }
+            }
+          }
+        })
+        web3auth.configureAdapter(openloginAdapter)
+
+        // plugins and adapters are optional and can be added as per your requirement
+        // read more about plugins here: https://web3auth.io/docs/sdk/web/plugins/
+
+        // adding torus wallet connector plugin
+
+        const torusPlugin = new TorusWalletConnectorPlugin({
+          torusWalletOpts: {},
+          walletInitOptions: {
+            whiteLabel: {
+              theme: { isDark: true, colors: { primary: "#00a8ff" } },
+              logoDark: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+              logoLight: "https://web3auth.io/images/w3a-D-Favicon-1.svg"
+            },
+            useWalletConnect: true,
+            enableLogging: true
+          }
+        })
+        console.log({ torusPlugin })
+        setTorusPlugin(torusPlugin)
+        await web3auth.addPlugin(torusPlugin)
+
+        // read more about adapters here: https://web3auth.io/docs/sdk/web/adapters/
+
+        // adding wallet connect v1 adapter
+        // const walletConnectV1Adapter = new WalletConnectV1Adapter({
+        //   adapterSettings: {
+        //     bridge: "https://bridge.walletconnect.org",
+        //   },
+        //   clientId,
+        // });
+
+        // web3auth.configureAdapter(walletConnectV1Adapter);
+
+        // adding wallet connect v2 adapter
+        const defaultWcSettings = await getWalletConnectV2Settings(
+          "eip155",
+          [1, 137, 5],
+          "04309ed1007e77d1f119b85205bb779d"
+        )
+        const walletConnectV2Adapter = new WalletConnectV2Adapter({
+          adapterSettings: { ...defaultWcSettings.adapterSettings },
+          loginSettings: { ...defaultWcSettings.loginSettings }
+        })
+
+        web3auth.configureAdapter(walletConnectV2Adapter)
+
+        // adding metamask adapter
+        const metamaskAdapter = new MetamaskAdapter({
+          clientId,
+          sessionTime: 3600, // 1 hour in seconds
+          web3AuthNetwork: "cyan",
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x1",
+            rpcTarget: "https://rpc.ankr.com/eth" // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          }
+        })
+        // we can change the above settings using this function
+        metamaskAdapter.setAdapterSettings({
+          sessionTime: 86400, // 1 day in seconds
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x1",
+            rpcTarget: "https://rpc.ankr.com/eth" // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+          web3AuthNetwork: "cyan"
+        })
+
+        // it will add/update  the metamask adapter in to web3auth class
+        web3auth.configureAdapter(metamaskAdapter)
+
+        const torusWalletAdapter = new TorusWalletAdapter({
+          clientId
+        })
+
+        // it will add/update  the torus-evm adapter in to web3auth class
+        web3auth.configureAdapter(torusWalletAdapter)
+
+        setWeb3auth(web3auth)
+
+        await web3auth.initModal()
+
+        setProvider(web3auth.provider)
+
+        if (web3auth.connected) {
+          setLoggedIn(true)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    init()
+  }, [])
+
   const getAllAccounts = async () => {
-    // EVM chains
-    const polygon_address = await getPolygonAddress();
-    const bnb_address = await getBnbAddress();
 
     // Solana
     let solana_address;
@@ -97,8 +264,6 @@ function App() {
     // const near_address = await getNearAddress();
 
     uiConsole(
-      "Polygon Address: " + polygon_address,
-      "BNB Address: " + bnb_address,
       "Solana Address: " + solana_address,
       "StarkEx Address: " + starkex_address,
       "StarkNet Address: " + starknet_address,
@@ -184,32 +349,6 @@ function App() {
     uiConsole(signedMessage);
   };
 
-  const getPolygonAddress = async () => {
-    if (!provider) {
-      uiConsole("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const privateKey = await rpc.getPrivateKey();
-
-    const polygonPrivateKeyProvider = new EthereumPrivateKeyProvider({
-      config: {
-        chainConfig: {
-          chainId: "0x13881",
-          rpcTarget: "https://rpc.ankr.com/polygon_mumbai",
-          displayName: "Polygon Mumbai",
-          blockExplorer: "https://mumbai.polygonscan.com/",
-          ticker: "MATIC",
-          tickerName: "MATIC",
-        },
-      },
-    });
-    await polygonPrivateKeyProvider.setupProvider(privateKey);
-    const web3 = new Web3(polygonPrivateKeyProvider.provider as any);
-    const address = (await web3.eth.getAccounts())[0];
-    return address;
-  };
-
   const getBnbAddress = async () => {
     if (!provider) {
       uiConsole("provider not initialized yet");
@@ -218,22 +357,7 @@ function App() {
     const rpc = new RPC(provider);
     const privateKey = await rpc.getPrivateKey();
 
-    const bnbPrivateKeyProvider = new EthereumPrivateKeyProvider({
-      config: {
-        chainConfig: {
-          chainId: "0x38",
-          rpcTarget: "https://rpc.ankr.com/bsc",
-          displayName: "Binance SmartChain Mainnet",
-          blockExplorer: "https://bscscan.com/",
-          ticker: "BNB",
-          tickerName: "BNB",
-        },
-      },
-    });
-    await bnbPrivateKeyProvider.setupProvider(privateKey);
-    const web3 = new Web3(bnbPrivateKeyProvider.provider as any);
-    const address = (await web3.eth.getAccounts())[0];
-    return address;
+    
   };
 
   const getSolanaAddress = async () => {
@@ -270,6 +394,15 @@ function App() {
     return solana_address[0];
   };
 
+
+  const showWCM = async () => {
+    if (!torusPlugin) {
+      uiConsole("torus plugin not initialized yet")
+      return
+    }
+    torusPlugin.showWalletConnectScanner()
+    uiConsole()
+  }
 
   // Will address this in future PR
   // const getNearAddress = async () => {
@@ -350,6 +483,11 @@ function App() {
         <div>
           <button onClick={getAllAccounts} className="card">
             Get All Accounts
+          </button>
+        </div>
+        <div>
+          <button onClick={showWCM} className="card">
+            Show Wallet Connect Modal
           </button>
         </div>
         <div>
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    environment: 'development',
+};
+
+export default config;
