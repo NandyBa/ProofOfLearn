3c25fe7bb7c39caadeaeb317044e01ced744a164
Nandy Bâ <nandyba@yahoo.fr>
2023-07-22 10:40:20 +0200

    save last quicknode post to json file to easy debugging

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 3000;
 
 // Middleware for parsing JSON and urlencoded form data
@@ -9,6 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.post('*', (req, res) => {
     console.log(`POST request received at: ${req.originalUrl}`);
     console.log('Body:', req.body);
+
+    // Write the body to lastData.json to easy debugging
+    fs.writeFileSync('lastData.json', JSON.stringify(req.body, null, '\t'));
+    
     res.status(200).send('Received POST data');
 });
 
