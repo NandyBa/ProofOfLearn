89b5e288479ed77ae2ec6e26d6feac2739e7dd93
L41TH <laith.elmershati@gmail.com>
2023-07-22 18:10:50 +0200

    add reward for experience completed

--- contracts/Puzzle.sol
+++ contracts/Puzzle.sol
@@ -6,22 +6,32 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Puzzle is ERC1155, Ownable {
     uint256 public immutable PIECE_NUMBER;
+    uint256 public pieceReward;
+    uint256 public finalPuzzleReward;
 
-    constructor(string memory uri, uint256 _PIECE_NUMBER) ERC1155(uri) {
+    constructor(string memory uri, uint256 _PIECE_NUMBER, uint256 _pieceReward, uint256 _finalPuzzleReward)
+        ERC1155(uri)
+    {
         PIECE_NUMBER = _PIECE_NUMBER;
+        pieceReward = _pieceReward;
+        finalPuzzleReward = _finalPuzzleReward;
     }
 
-    function setURI(string memory newuri) public onlyOwner {
+    function setURI(string memory newuri) external onlyOwner {
         _setURI(newuri);
     }
 
-    function mint(address account, uint256 id) external onlyOwner {
+    function mint(address account, uint256 id) external payable onlyOwner {
+        require(msg.value == pieceReward, "incorrect amount");
         require(id < PIECE_NUMBER, "Exceed number of pieces");
         require(balanceOf(account, id) == 0, "token already in possession");
         _mint(account, id, 1, "");
+
+        (bool ok,) = payable(address(this)).call{value: pieceReward}("");
+        require(ok, "call failed");
     }
 
-    function claimProofOfLearn(address account) public {
+    function claimProofOfLearn(address account) external {
         uint256 id = PIECE_NUMBER + 1;
         require(balanceOf(account, id) == 0, "token already in possession");
 
