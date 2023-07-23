f714be024c63aae3c08559ae915b118da32e7992
L41TH <laith.elmershati@gmail.com>
2023-07-23 06:54:11 +0200

    run figma app

--- backend/QuickNode/QuickNodeRequestHeader.js
+++
@@ -1,8 +0,0 @@
-const { QuickNode_API_Key } = require('../config')
-
-var headers= new Object();
-headers['Accept'] = 'application/json';
-headers['Content-Type'] = 'application/json';
-headers['x-api-key'] = QuickNode_API_Key;
-
-module.exports = headers;
